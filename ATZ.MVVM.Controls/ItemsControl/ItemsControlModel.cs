ba2018eab09667b5a7b1491d59d12c5643c480a7
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-01-01 02:15:56 +1100

    ItemsControl added.

---
+++ ATZ.MVVM.Controls/ItemsControl/ItemsControlModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+using ATZ.MVVM.Controls.Control;
+
+namespace ATZ.MVVM.Controls.ItemsControl
+{
+    public class ItemsControlModel : ControlModel
+    {
+        public ObservableCollection<object> Items { get; } = new ObservableCollection<object>();
+    }
+}
---
+++ ATZ.MVVM.Controls/ItemsControl/ItemsControlViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+using ATZ.MVVM.Controls.Control;
+using ATZ.MVVM.ViewModels.Utility;
+using ATZ.MVVM.ViewModels.Utility.Connectors;
+
+namespace ATZ.MVVM.Controls.ItemsControl
+{
+    public class ItemsControlViewModel : ControlViewModel, IViewModel<ItemsControlModel>
+    {
+        internal ObservableCollection<IViewModel<object>> ItemsCollection { get; } = new ObservableCollection<IViewModel<object>>();
+        public CollectionViewModelToModelConnector<object> Items { get; private set; }
+
+        protected override void BindModel()
+        {
+            base.BindModel();
+
+            Items = new CollectionViewModelToModelConnector<object>
+            {
+                ModelCollection = GetModel().Items,
+                ViewModelCollection = ItemsCollection
+            };
+        }
+
+        public new ItemsControlModel GetModel()
+        {
+            return (ItemsControlModel) Model;
+        }
+
+        public void SetModel(ItemsControlModel model)
+        {
+            Model = model;
+            Items.ModelCollection = model.Items;
+        }
+    }
+}
