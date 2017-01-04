e6c4424ca48cae576191ff1c87fd35b5b1225654
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-01-05 07:57:15 +1100

    Items show up in TreeView.

--- ATZ.MVVM.Controls.Wpf/TreeViewView.xaml.cs
+++ ATZ.MVVM.Controls.Wpf/TreeViewView.xaml.cs
@@ -1,16 +1,21 @@
 ﻿using ATZ.MVVM.Controls.FrameworkElement;
 using ATZ.MVVM.Controls.TreeView;
 using ATZ.MVVM.ViewModels.Utility;
+using ATZ.MVVM.Views.Utility.Connectors;
 using ATZ.MVVM.Views.Utility.Interfaces;
 using System.Windows;
 
 namespace ATZ.MVVM.Controls.Wpf
 {
+    using TItemsConnector = CollectionViewToViewModelConnector<IView<IViewModel<object>>, object>;
+
     /// <summary>
     /// Interaction logic for TreeViewView.xaml
     /// </summary>
     public partial class TreeViewView : IView<IViewModel<TreeViewModel>>, IView<IViewModel<FrameworkElementModel>>
     {
+        private TItemsConnector _itemsConnector;
+
         public UIElement UIElement => this;
 
         public TreeViewView()
@@ -20,7 +25,12 @@ namespace ATZ.MVVM.Controls.Wpf
 
         private void BindModelImplementation(IViewModel<TreeViewModel> vm)
         {
-
+            var tvvm = (TreeViewViewModel)vm;
+            _itemsConnector = new TItemsConnector
+            {
+                ViewModelCollection = tvvm.ItemsCollection,
+                ViewCollection = Items
+            };
         }
 
         public void BindModel(IViewModel<FrameworkElementModel> vm) => BindModelImplementation((IViewModel<TreeViewModel>)vm);
